47507d51dbb7700802dc2877d1b9bfff029147e3
moroccanoid <adambentahar15@gmail.com>
2024-04-21 19:43:57 -0400

    Update script.js

--- src/client/goals/script.js
+++ src/client/goals/script.js
@@ -1,9 +1,27 @@
+import { UserDB } from "../DBManager/UserDB";
+
+const userDB = new UserDB('goals');
+
+window.addEventListener('DOMContentLoaded', async function() {
+    const savedGoals = await userDB.loadAll();
+    savedGoals.forEach(function(goal) {
+        addGoalToDOM(goal);
+    });
+});
+
 document.getElementById('enterGoalButton').addEventListener('click', function() {
     let wpmGoal = document.getElementById('wpmGoal').value;
     let txtGoal = document.getElementById('nt').value;
 
+    let goal = {wpmGoal: wpmGoal, textsGoal: txtGoal};
+
+    addGoalToDOM(goal);
+    userDB.save('goal_' + Date.now(), goal);
+});
+
+function addGoalToDOM(goal) {
     let newGoal = document.createElement('div');
     newGoal.classList.add('newCurrentGoal');
-    newGoal.innerHTML = '<h3>Words Per Minute Goal: ' + wpmGoal + '</h3><h3>Number of Texts Goal: ' + txtGoal + '</h3>';
+    newGoal.innerHTML = '<h3>Words Per Minute Goal: ' + goal.wpmGoal + '</h3><h3>Number of Texts Goal: ' + goal.textsGoal + '</h3>';
     document.querySelector('.current-goal').appendChild(newGoal);
-});
+}
