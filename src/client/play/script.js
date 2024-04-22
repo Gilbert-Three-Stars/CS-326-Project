ef457990598e830b064be2c5208e836314f6d22c
TrongTheFirst <trongvo018@gmail.com>
2024-04-21 22:58:36 -0400

    better structure

--- src/client/play/script.js
+++ src/client/play/script.js
@@ -3,10 +3,72 @@ const textEntry = document.querySelector('.text-entry');
 const wpmDisplay = document.getElementById('wpm');
 const accuracyDisplay = document.getElementById('acc');
 const timeDisplay = document.getElementById("time");
+const endTimeDisplay = document.getElementById("timer");
+const creditsDisplay = document.getElementById("credits");
 const newTextBtn = document.getElementById("new-text");
 const restartBtn = document.getElementById('restart');
 
-textEntry.addEventListener("input", () => {
+
+//disables pasting
+textEntry.addEventListener('paste', (event) => {
+    event.preventDefault();
+});
+
+textEntry.addEventListener("input",(event)=>{
+    gameRunning(event);
+})
+// textEntry.addEventListener("input", () => {
+//     if(!timerRunning){
+//         startTimer();
+//     }
+//     const quote = quoteText.textContent.trim();
+//     const entry = textEntry.textContent.trim();
+
+//     if(entry === quote) {
+//         textEntry.style.backgroundColor = 'rgba(115, 227, 84, 0.5)';
+//         endGame();
+//     }
+//     else { // check if user is on the right track
+//         let shavedQuote = quote.substring(0, entry.length);
+//         if(entry === shavedQuote) {
+//             textEntry.style.backgroundColor = 'rgb(33, 33, 33)';
+//         } // if user is not on right track display in red
+//         else {
+//             textEntry.style.backgroundColor = 'rgba(204, 71, 61, 0.5)';
+//         }
+//     }
+// })
+
+
+// textEntry.addEventListener('input', function(event) {
+//     const quote = quoteText.textContent.trim();
+//     const entry = textEntry.textContent.trim();
+//     if (event.inputType === 'deleteContentBackward') {
+//         // Ignore backspace input
+//         return;
+//     }
+//     if (entry.charAt(entry.length - 1) !== quote.charAt(entry.length - 1)) {
+//         mistakeMade++;
+//     }
+//     // Calculate accuracy: (total characters typed - mistakes) / total characters typed
+//     const accuracy = Math.max(0, ((entry.length - mistakeMade) / entry.length) * 100);
+//     if (!startTime) {
+//         startTime = Date.now();
+//     }
+//     const words = entry.split(/\s+/);
+//     wordCount = words.length;
+//     const timeElapsed = (Date.now() - startTime) / (1000 * 60);
+//     const wpm = Math.round(wordCount / timeElapsed);
+//     accuracyDisplay.textContent = `Accuracy: ${accuracy.toFixed(2)}%`;
+//     wpmDisplay.textContent = `WPM: ${wpm}`;
+// });
+
+
+let mistakeMade = 0
+let startTime;
+let wordCount = 0; 
+function gameRunning(event){
+    //tracks mistakes
     if(!timerRunning){
         startTimer();
     }
@@ -26,15 +88,8 @@ textEntry.addEventListener("input", () => {
             textEntry.style.backgroundColor = 'rgba(204, 71, 61, 0.5)';
         }
     }
-})
 
-//tracks wpms, time, and accuracy
-let mistakeMade = 0
-let startTime;
-let wordCount = 0; 
-textEntry.addEventListener('input', function(event) {
-    const quote = quoteText.textContent.trim();
-    const entry = textEntry.textContent.trim();
+    ////tracks wpms, time, and accuracy
     if (event.inputType === 'deleteContentBackward') {
         // Ignore backspace input
         return;
@@ -53,8 +108,7 @@ textEntry.addEventListener('input', function(event) {
     const wpm = Math.round(wordCount / timeElapsed);
     accuracyDisplay.textContent = `Accuracy: ${accuracy.toFixed(2)}%`;
     wpmDisplay.textContent = `WPM: ${wpm}`;
-});
-
+}
 let timer;
 let sec = 180;
 let timerRunning = false;
@@ -80,28 +134,35 @@ function stopTimer(){
 function endGame(){
     textEntry.contentEditable = false;
     textEntry.blur();
+    creditsDisplay.innerHTML = `By: ${credits}`;
+    const minutes = Math.floor((180-sec) / 60);
+    let remainingSeconds = (180-sec) % 60;
+    if(remainingSeconds<10) remainingSeconds= "0"+remainingSeconds;
+    if(remainingSeconds===0)remainingSeconds="00";
+    endTimeDisplay.innerHTML = `Time: ${minutes}:${remainingSeconds}`;
+    document.getElementById("endgame-stats-display").style.display = 'flex';
     stopTimer();
 }
+
 const response = await fetch("quotes.csv");
 const csvText = await response.text();
 const parsedText = Papa.parse(csvText).data;
-
-// console.log(text[236719])
-
 async function generateText(){
     let randomIndex = Math.floor(Math.random() * parsedText.length);
     return new Promise((resolve, reject) => { resolve(parsedText[randomIndex])});
 }
-
+let credits;
 async function startRound(){
     restart();
     generateText().then(text=>{
+        credits = text[1];
         quoteText.textContent = text[0].replace("’","'");
     })
 }
 
 function restart(){
     stopTimer();
+    document.getElementById("endgame-stats-display").style.display = 'none';
     textEntry.innerHTML = "";
     textEntry.style.backgroundColor = 'rgb(33, 33, 33)';
     textEntry.contentEditable = true;
