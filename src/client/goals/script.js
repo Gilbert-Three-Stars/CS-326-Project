45cb18feb7ef85e0c69f77f710407dddbf978b70
moroccanoid <adambentahar15@gmail.com>
2024-04-24 22:51:41 -0400

    Update script.js

--- src/client/goals/script.js
+++ src/client/goals/script.js
@@ -1,35 +1,5 @@
 import {UserDB} from "../DBManager/UserDB.js";
 
-
-
-// window.addEventListener('DOMContentLoaded', async function() {
-//     const savedGoals = await userDB.loadAll();
-//     savedGoals.forEach(function(goal) {
-//         addGoalToDOM(goal);
-//     });
-// });
-
-// document.getElementById('enterGoalButton').addEventListener('click', function() {
-//     let wpmGoal = document.getElementById('wpmGoal').value;
-//     let txtGoal = document.getElementById('nt').value;
-
-//     let goal = {wpmGoal: wpmGoal, textsGoal: txtGoal};
-
-//     addGoalToDOM(goal);
-//     userDB.save('goal_' + Date.now(), goal);
-// });
-
-// function addGoalToDOM(goal) {
-//     let newGoal = document.createElement('div');
-//     newGoal.classList.add('newCurrentGoal');
-//     newGoal.innerHTML = '<h3>Words Per Minute Goal: ' + goal.wpmGoal + '</h3><h3>Number of Texts Goal: ' + goal.textsGoal + '</h3>';
-//     document.querySelector('.current-goal').appendChild(newGoal);
-// }
-
-
-
-
-
 //This is the code from stats. you can change it accordingly to fit this page.
 
 //if you want to access any of the data you created. Go to website, then stats->admin
@@ -38,29 +8,21 @@ let db = new UserDB();
 
 const wpmInput = document.getElementById("wpmGoal");
 const textInput = document.getElementById("nt");
-//const wpmBtn = document.getElementById("enterWPMButton");
-//const textBtn = document.getElementById("enterTextButton");
 const enterGoalButton = document.getElementById("enterGB");
 const currentGoal = document.getElementById("current-goal");
 const previousGoal = document.getElementById("previous-goal");
 const processResButton = document.getElementById("processRes");
 
+//uploads user inputted goal to current goal upon clicking enter goal button
 enterGoalButton.addEventListener("click",async()=>{
     await createData("wpmGoals", wpmInput.value);
     await createData("textGoals", textInput.value);
-    await createData("prevWpmGoals", wpmInput.value);
-    await createData("prevTextGoals", textInput.value);
 });
-/*textBtn.addEventListener("click",async()=>{
-    await createData("textGoals",textInput.value);
-})*/
 
-//THIS IS CODE TO READ THE DATA
-
+//This is code to process data, checks to see if current data matches goal. Uploads current goal data to previous goal if goals are met
 processResButton.addEventListener("click", async () => {
     const data = await db.load("runs");
     const runs = data.data;
-    //processRun(runs);
     let runTime = 0;
     let topSpeed = 0;
     let totalSpeed = 0;
@@ -75,6 +37,7 @@ processResButton.addEventListener("click", async () => {
         if(run.wpm>topSpeed) topSpeed=run.wpm;
         if(run.acc>topAcc) topAcc=run.acc;
     });
+    //calculates average speed (words per minute over number of texts)
     let avg = totalSpeed/numRuns;
 
     const wpmGoalData = await db.load("wpmGoals");
@@ -83,14 +46,19 @@ processResButton.addEventListener("click", async () => {
     const wpmGoal = wpmGoalData.data;
     const textGoal = textGoalData.data;
         if((avg >= wpmGoal) && (numRuns >= textGoal)){
-            //goal achieved
+            //current goal has been achieved
             try{
-                // await db.save("prevWpmGoals", wpmGoal);
-                // await db.save("prevTextGoals", textGoal);
-                await updateData("prevWpmGoals", wpmGoal);
-                await updateData("prevTextGoals", textGoal);
+                try{
+                    await db.save("prevWpmGoals", wpmGoal);
+                    await db.save("prevTextGoals", textGoal);
+                }
+                catch(err){
+                    await updateData("prevWpmGoals", wpmGoal);
+                    await updateData("prevTextGoals", textGoal);
+                }
                 await db.delete("wpmGoals");
                 await db.delete("textGoals");
+                //currentGoal.innerHTML = "You have no current goals, enter one!"; 
             }
             catch(error){
                 alert("something is wrong");
@@ -102,40 +70,6 @@ processResButton.addEventListener("click", async () => {
     viewAll();
 });
 
-/*async function checkAchievements(avg){
-    if((avg >= wpmInput.value) && (numRuns >= textInput.value)){
-        //goal achieved
-        let wpm = await db.load("wpmGoals");
-        previousGoal.innerHTML = `WPM Goals: ${JSON.stringify(wpm.data)}`;
-        let texts = await db.load("textGoals");
-        previousGoal.innerHTML += `<br>Text Goals: ${JSON.stringify(texts.data)}`;
-        db.delete("wpmGoals");
-        db.delete("textGoals");
-        currentGoal.innerHTML = "";
-        currentGoal.innerHTML = "";
-        numRuns = 0;
-        avg = 0;
-    }
-}
-
-async function processRun(runs){
-    let runTime = 0;
-    let topSpeed = 0;
-    let totalSpeed = 0;
-    let topAcc = 0;
-    let totalAcc = 0;
-    let numRuns = 0;
-    runs.forEach(run =>{
-        runTime+=run.runTime;
-        totalSpeed+=run.wpm;
-        totalAcc+=parseFloat(run.acc);
-        numRuns++;
-        if(run.wpm>topSpeed) topSpeed=run.wpm;
-        if(run.acc>topAcc) topAcc=run.acc;
-    });
-    let avg = totalSpeed/numRuns;
-    //checkAchievements(avg);
-}*/
 
 async function createData(name,data) {
     if(!name || !data){
@@ -150,18 +84,6 @@ async function createData(name,data) {
     }
     viewAll();
 }
-
-
-// async function readData(name) {
-//     if(!name){
-//         alert("Name is required!");
-//     }else{
-//         try{
-//             const data = await db.load(name);
-//             response.innerHTML = JSON.stringify(data);
-//         }catch(error){console.log(error);}
-//     }
-// }
 
 
 async function updateData(name,newData) {
@@ -181,30 +103,15 @@ async function updateData(name,newData) {
 
 }
 
-// async function deleteData(name) {
-//     if(!name){
-//         alert("Name is required!");
-//     }else{
-//         try{
-//             await db.delete(name);
-//         }catch(error){}
-//         viewAll();
-//     }
-// }
-
 async function viewAll() {
     try{
-        // const all = await db.loadAll();
-        // let responseText = "";
-        // all.forEach((data) => {
-        //   responseText += `${data._id} = ${JSON.stringify(data.data)}<br>`;
-        // });
-        // currentGoal.innerHTML = responseText;
-
-        let wpm = await db.load("wpmGoals");
-        currentGoal.innerHTML = `WPM Goals: ${JSON.stringify(wpm.data)}`;
-        let texts = await db.load("textGoals");
-        currentGoal.innerHTML += `<br>Text Goals: ${JSON.stringify(texts.data)}`;
+        try{
+            let wpm = await db.load("wpmGoals");
+            currentGoal.innerHTML = `WPM Goals: ${JSON.stringify(wpm.data)}`;
+            let texts = await db.load("textGoals");
+            currentGoal.innerHTML += `<br>Text Goals: ${JSON.stringify(texts.data)}`;
+        }
+        catch(err){}
         let prevWpm = await db.load("prevWpmGoals");
         previousGoal.innerHTML = `WPM Goals: ${JSON.stringify(prevWpm.data)}`;
         let prevTexts = await db.load("prevTextGoals");
