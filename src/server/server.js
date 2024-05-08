c335a5dee0b749e1f799352d6b50db36b51b25a7
TrongTheFirst <trongvo018@gmail.com>
2024-05-08 16:27:54 -0400

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -7,14 +7,14 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'client')));
-app.use('/home', express.static(path.join(__dirname, 'client', 'home')));
-app.use('/play', express.static(path.join(__dirname, 'client', 'play')));
-app.use('/stats', express.static(path.join(__dirname, 'client', 'stats')));
-app.use('/goals', express.static(path.join(__dirname, 'client', 'goals')));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/home', express.static(path.join(__dirname, '..client', 'home')));
+app.use('/play', express.static(path.join(__dirname, '../client', 'play')));
+app.use('/stats', express.static(path.join(__dirname, '../client', 'stats')));
+app.use('/goals', express.static(path.join(__dirname, '../client', 'goals')));
 // Define route handlers
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client','home','index.html'));
+  res.sendFile(path.join(__dirname, '../client','home','index.html'));
 });
   
 app.listen(port, () => {
